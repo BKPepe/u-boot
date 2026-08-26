75219d1fc0693af63dfed5130a211e5726f92b04
Josef Schlehofer <pepe.schlehofer@gmail.com>
2026-08-26 12:25:17 +0000

    scripts: gen_ll_addressable_symbols: Skip undefined symbols
    Only generate __ADDRESSABLE() entries for linker list symbols defined by
    the input objects. Undefined symbols can otherwise introduce hard
    references to drivers that are not part of the SPL.
    
    Signed-off-by: Josef Schlehofer <pepe.schlehofer@gmail.com>

--- scripts/gen_ll_addressable_symbols.sh
+++ scripts/gen_ll_addressable_symbols.sh
@@ -11,6 +11,6 @@
 set -e
 
 echo '#include <linux/compiler.h>'
-$@ 2>/dev/null | grep -oe '_u_boot_list_2_[a-zA-Z0-9_]*_2_[a-zA-Z0-9_]*' \
+$@ 2>/dev/null | grep -v ' U ' | grep -oe '_u_boot_list_2_[a-zA-Z0-9_]*_2_[a-zA-Z0-9_]*' \
 	-e '__stack_chk_guard' | sort -u | \
 	sed -e 's/^\(.*\)/extern char \1[];\n__ADDRESSABLE(\1);/'
