BKPepe/u-boot

commit 98dfa70f84f036c7423f1eddf00af8ea555fed8f

fsl: mxc: Drop legacy I2c
Drop some old code from the mxc_i2c and ddr/fsl drivers. This will allow
removal of very old common I2C code.

Signed-off-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Heiko Schocher <hs@denx.de>

drivers/ddr/fsl/main.c
@@ -111,7 +111,7 @@ static int ddr_i2c_read(DEV_TYPE *dev, unsigned int addr,
 #if CONFIG_IS_ENABLED(DM_I2C)
 	ret = dm_i2c_read(dev, 0, buf, len);
 #else
-	ret = i2c_read(dev->chip, addr, alen, buf, len);
+	ret = 0;
 #endif
 
 	return ret;
@@ -162,7 +162,6 @@ static void __get_spd(generic_spd_eeprom_t *spd, u8 i2c_address)
 	};
 	dev = &ldev;
 
-	i2c_set_bus_num(CONFIG_SYS_SPD_BUS_NUM);
 #endif
 
 #ifdef CONFIG_SYS_FSL_DDR4

drivers/i2c/mxc_i2c.c
@@ -620,6 +620,7 @@ int enable_i2c_clk(unsigned char enable, unsigned int i2c_num)
 	__attribute__((weak, alias("__enable_i2c_clk")));
 
 #if !CONFIG_IS_ENABLED(DM_I2C)
+
 /*
  * Read data from I2C device
  *

commit b7a831cadea9d1c81247aa55eee1e34fd48fda35

ddr: fsl: Fix SPD read when legacy I2C is used
Commit 98dfa70f84f0 ("fsl: mxc: Drop legacy I2c") replaced the legacy
I2C branch of ddr_i2c_read() with a stub that returns 0 without
reading anything. Since then every board that reads its SPD EEPROM
through the legacy I2C interface, which includes all mpc85xx SPL
builds as SPL_DM_I2C is not available there, gets an all-zero SPD
buffer together with a success return value and dies with:

  Error: No valid SPD detected.

Restore the legacy read.

Tested on CZ.NIC Turris 1.1 (P2020, SD boot): SPL detects the DDR3
SO-DIMM again (2 GiB, CL=6).

Fixes: 98dfa70f84f0 ("fsl: mxc: Drop legacy I2c")
Signed-off-by: Josef Schlehofer <pepe.schlehofer@gmail.com>

drivers/ddr/fsl/main.c
@@ -111,7 +111,7 @@ static int ddr_i2c_read(DEV_TYPE *dev, unsigned int addr,
 #if CONFIG_IS_ENABLED(DM_I2C)
 	ret = dm_i2c_read(dev, 0, buf, len);
 #else
-	ret = 0;
+	ret = i2c_read(dev->chip, addr, alen, buf, len);
 #endif
 
 	return ret;